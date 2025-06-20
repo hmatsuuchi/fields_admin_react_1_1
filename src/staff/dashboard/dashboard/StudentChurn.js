8f979bc46d9cc7046742fece972102503bf32369
Hiroki Matsuuchi <hmatsuuchi@gmail.com>
2025-06-20 10:24:03 +0900

    visual changes to total active student widget

--- src/staff/dashboard/dashboard/StudentChurn.js
+++ src/staff/dashboard/dashboard/StudentChurn.js
@@ -1,9 +1,8 @@
 import React, { useEffect } from "react";
-
 /* AXIOS */
 import instance from "../../../axios/axios_authenticated";
 /* CHART JS */
-import { Bar, Line } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   BarElement,
--- src/staff/dashboard/dashboard/TotalActiveStudents.js
+++ src/staff/dashboard/dashboard/TotalActiveStudents.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 /* AXIOS */
 import instance from "../../../axios/axios_authenticated";
+/* CHART JS */
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 /* COMPONENTS */
 import LoadingSpinner from "../../micro/LoadingSpinner";
 /* CSS */
@@ -12,10 +15,8 @@ function TotalActiveStudents() {
   /* ------------------------------------------- */
 
   const [totalActiveStudentsCount, setTotalActiveStudentsCount] = useState(0);
-
-  const [highestActiveStudentCount, setHighestActiveStudentCount] =
-    useState(null);
-
+  const [highestActiveStudentCount, setHighestActiveStudentCount] = useState(0);
+  const [highestActiveStudentDate, setHighestActiveStudentDate] = useState("");
   /* ----------------------------------------------- */
   /* ------------------ FUNCTIONS ------------------ */
   /* ----------------------------------------------- */
@@ -27,7 +28,10 @@ function TotalActiveStudents() {
       .then((response) => {
         setTotalActiveStudentsCount(response.data.total_active_students_count);
         setHighestActiveStudentCount(
-          response.data.highest_active_student_count
+          response.data.highest_active_student_count.count
+        );
+        setHighestActiveStudentDate(
+          response.data.highest_active_student_count.date
         );
       })
       .catch((error) => {
@@ -41,24 +45,75 @@ function TotalActiveStudents() {
     fetchData();
   }, []);
 
+  /* doughnut chart */
+  ChartJS.register(ArcElement, Tooltip, Legend);
+
+  const data = {
+    labels: ["Current", "Shortfall"],
+    datasets: [
+      {
+        label: "Students",
+        data: [
+          totalActiveStudentsCount,
+          Math.min(
+            (highestActiveStudentCount - totalActiveStudentsCount) * 10,
+            highestActiveStudentCount * 5
+          ), // exaggerates the shortfall for visual effect but limits it to a maximum of 5x the highest count
+        ],
+        backgroundColor: ["#00b8a9", "#ffde7d"],
+        borderWidth: 0,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+
+    plugins: {
+      tooltip: { enabled: false },
+      legend: {
+        display: false,
+      },
+    },
+    hover: { mode: null }, // disables hover effects
+    cutout: "70%", // makes the doughnut chart look like a ring
+  };
+
   /* ---------------------------------------- */
   /* -----------------  JSX ----------------- */
   /* ---------------------------------------- */
 
   return (
     <div id="total-active-students" className="component-primary-container">
-      <div className="component-title">TOTAL ACTIVE STUDENTS</div>
-      {highestActiveStudentCount ? (
-        <div className="total-active-students-data-container">
-          CURRENT ACTIVE STUDENTS: {totalActiveStudentsCount}
-          <br />
-          HIGHEST COUNT: {highestActiveStudentCount.count}
-          <br />
-          HIGHTEST DATE: {highestActiveStudentCount.date.slice(0, 10)}
-          <br />
-          *** active students are defined as students who have two or more
-          present attendance records and their most recent present attendance
-          record is within the last 28 days ***
+      <div className="component-title">在籍生徒数</div>
+      {totalActiveStudentsCount !== 0 &&
+      highestActiveStudentCount !== 0 &&
+      highestActiveStudentDate !== "" ? (
+        <div className="total-active-students-content-container">
+          <div className="data-container">
+            <div className="current-count-container">
+              <div
+                className={`count${
+                  highestActiveStudentCount - totalActiveStudentsCount !== 0
+                    ? " under"
+                    : ""
+                }`}
+              >
+                {totalActiveStudentsCount}
+              </div>
+              <div className="label">人</div>
+            </div>
+            <div className="highest-count-container">
+              <div className="count">{highestActiveStudentCount}</div>
+              <div className="label">人</div>
+            </div>
+            <div className="highest-date">
+              {highestActiveStudentDate.slice(0, 10)}
+            </div>
+          </div>
+          <div className="chart-container">
+            <Doughnut data={data} options={options} />
+          </div>
         </div>
       ) : (
         <LoadingSpinner />
