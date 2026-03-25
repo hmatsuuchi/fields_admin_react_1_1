82760dedb5f105a110c15f3e6a884d761727c8bf
Hiroki Matsuuchi <hmatsuuchi@gmail.com>
2026-03-25 11:49:58 +0900

    MVP - Incomplete Attendance for All Instructors

--- src/staff/dashboard/overview/IncompleteAttendanceForAllInstructors.js
+++ src/staff/dashboard/overview/IncompleteAttendanceForAllInstructors.js
@@ -9,6 +9,9 @@ function IncompleteAttendanceForAllInstructors() {
   /* ------------------ STATE ------------------ */
   /* ------------------------------------------- */
 
+  const [attendanceAll, setAttendanceAll] = React.useState([]);
+  const [instructorData, setInstructorData] = React.useState([]);
+
   /* ----------------------------------------------- */
   /* ------------------ FUNCTIONS ------------------ */
   /* ----------------------------------------------- */
@@ -19,7 +22,10 @@ function IncompleteAttendanceForAllInstructors() {
       .get("api/dashboard/dashboard/incomplete_attendance_for_all_instructors/")
       .then((response) => {
         if (response) {
-          console.log(response.data);
+          setAttendanceAll(response.data.attendance_data);
+          setInstructorData(response.data.instructor_data);
+          console.log(response.data.attendance_data);
+          console.log(response.data.instructor_data);
         }
       })
       .catch((error) => {
@@ -38,10 +44,35 @@ function IncompleteAttendanceForAllInstructors() {
   /* ---------------------------------------- */
 
   return (
-    <div
-      id="incomplete-attendance-for-all-instructors-container"
-      onClick={() => fetchData()}
-    ></div>
+    <div id="incomplete-attendance-for-all-instructors-container">
+      {instructorData.map((instructor) => (
+        <div
+          className="instructor-container"
+          key={`instructor-id-${instructor.id}`}
+        >
+          <div className="header">
+            {instructor.instructor_id} -{" "}
+            {instructor.instructor__userprofilesinstructors__last_name_romaji},{" "}
+            {instructor.instructor__userprofilesinstructors__first_name_romaji}
+          </div>
+          <div className="attendance-container">
+            {attendanceAll.map((item) => {
+              if (item.instructor.id === instructor.instructor_id) {
+                return (
+                  <div
+                    className={`attendance-item${item.incomplete_count > 0 ? " incomplete" : ""}`}
+                    key={`item-id-${item.id}`}
+                  >
+                    {`${item.date} [ ${item.incomplete_count} ]`}
+                  </div>
+                );
+              }
+              return null;
+            })}
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
 
